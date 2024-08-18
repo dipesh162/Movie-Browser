6f18c3f37ba342d7c40fa3928b8f5da9ab41d39a
dipesh162 <dipeshsingh162@gmail.com>
2024-08-19 00:29:28 +0530

    Fixing prod bug - 00:29

--- src/app/components/Bookmark.js
+++ src/app/components/Bookmark.js
@@ -1,36 +1,44 @@
 'use client';
 
-import Image from 'next/image'
-import React, { useState } from 'react'
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
 
+function Bookmark({ id }) {
+    const [isSaved, setSaved] = useState(false);
 
-function Bookmark({id}) {
-    const savedItems = JSON.parse(global?.window?.localStorage.getItem('saved')) || [];
-    const [isSaved, setSaved] = useState(savedItems?.includes(id))
-    
-    const handleClick = ()=>{
+    useEffect(() => {
+        // Only runs on the client side
+        const savedItems = JSON.parse(window.localStorage.getItem('saved')) || [];
+        setSaved(savedItems.includes(id));
+    }, [id]);
+
+    const handleClick = () => {
         const savedItems = JSON.parse(window.localStorage.getItem('saved')) || [];
 
-        if(isSaved){
-            let newSavedItems = savedItems.filter((savedId,i)=> savedId!==id)
-            window?.localStorage?.setItem('saved', JSON.stringify(newSavedItems))
+        if (isSaved) {
+            const newSavedItems = savedItems.filter((savedId) => savedId !== id);
+            window.localStorage.setItem('saved', JSON.stringify(newSavedItems));
             setSaved(false);
         } else {
             const newSavedItems = [...savedItems, id];
             window.localStorage.setItem('saved', JSON.stringify(newSavedItems));
             setSaved(true);
         }
-    }
+    };
 
     return (
-        <div onClick={handleClick} className='cursor-pointer absolute right-0 top-0 z-[1] rounded-bl-xl w-[45px] h-[45px] flex items-center justify-center bg-white'>
+        <div
+            onClick={handleClick}
+            className='cursor-pointer absolute right-0 top-0 z-[1] rounded-bl-xl w-[45px] h-[45px] flex items-center justify-center bg-white'
+        >
             <Image
                 src={`/images/${isSaved ? 'bookmark_filled.png' : 'bookmark_empty.png'}`}
                 width={25}
                 height={25}
+                alt={isSaved ? 'Saved' : 'Not Saved'}
             />
         </div>
-    )
+    );
 }
 
-export default Bookmark
+export default Bookmark;
