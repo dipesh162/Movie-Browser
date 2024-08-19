c49a92ee51c1fe64289d3c94a0d8ea3d3eed2602
dipesh162 <dipeshsingh162@gmail.com>
2024-08-19 16:04:03 +0530

    checking prod fix

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,8 @@ export default async function Home() {
   // Pass data to Client Component
   return (
     <main className="max-w-[95%] md:max-w-[75%] m-auto flex flex-col gap-4 px-3 md:px-6 py-4 md:py-8">
-      <Main initialData={data} initialError={error} initialQueryParams={initialQueryParams} />
+      <h2>hh</h2>
+      {/* <Main initialData={data} initialError={error} initialQueryParams={initialQueryParams} /> */}
     </main>
   );
 }
