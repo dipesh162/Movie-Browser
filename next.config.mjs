d7e959ebe2b1f4af10bb54d29c9db0d2f63f7c35
dipesh162 <dipeshsingh162@gmail.com>
2024-08-19 16:55:21 +0530

    Fixed next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        remotePatterns: [
+            {
+              protocol: 'https',
+              port: '',
+              pathname: '/**',
+            },
+        ],
         domains: ['image.tmdb.org'],
     },  
 };
